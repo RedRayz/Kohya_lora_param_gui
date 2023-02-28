56431d3e492a5a7f3d8a7dbf0570271e3cf67ced
RedRayz <71994877+RedRayz@users.noreply.github.com>
2023-02-28 21:47:37 +0900

    プロジェクト ファイルを追加します。

---
+++ Kohya_lora_trainer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kohya_lora_trainer
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
