abcbf10efd0b944be0c0ba7287551bbf6c07a340
RedRayz <71994877+RedRayz@users.noreply.github.com>
2024-07-15 06:38:56 +0900

    Add dataset tools form

---
+++ Kohya_lora_trainer/FormDatasetTools.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Kohya_lora_trainer
+{
+    partial class FormDatasetTools
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // FormDatasetTools
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(689, 397);
+            Font = new Font("Yu Gothic UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "FormDatasetTools";
+            Text = "データセット編集/選別ツール";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
