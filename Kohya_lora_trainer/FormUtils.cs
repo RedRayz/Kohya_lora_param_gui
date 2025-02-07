dea5f11df2abc04f6c7a2607451f9b60e2e6d379
RedRayz <71994877+RedRayz@users.noreply.github.com>
2025-02-08 02:30:23 +0900

    Revert "Set index-url for installing xformers"
    This reverts commit bacba67f8049a3fb93e4ef6587a82d724c7f9fe1.

--- Kohya_lora_trainer/FormUtils.cs
+++ Kohya_lora_trainer/FormUtils.cs
@@ -228,9 +228,7 @@ namespace Kohya_lora_trainer
                     .Append(vision).Append(" --index-url ")
                     .Append(index)
                     .Append(" && pip install --upgrade -r requirements.txt && pip install xformers==")
-                    .Append(xformers).Append(" --index-url ")
-                    .Append(index)
-                    .Append(" && ");
+                    .Append(xformers).Append(" && ");
 
                 sb.Append("pip install dadaptation lycoris_lora");
                 ProcessStartInfo ps = new ProcessStartInfo();
