a091057ecce6f9bf02f35519182097bd6cc4c23c
RedRayz <71994877+RedRayz@users.noreply.github.com>
2025-02-08 19:33:43 +0900

    Install numpy==1.26.4 to fix api not found error

--- Kohya_lora_trainer/FormInstaller.cs
+++ Kohya_lora_trainer/FormInstaller.cs
@@ -46,7 +46,7 @@ namespace Kohya_lora_trainer
                 .Append(" && pip install --upgrade -r requirements.txt && pip install xformers==")
                 .Append(Constants.XFORMERS_VERSION).Append(" && ");
 
-            sb.Append("pip install dadaptation lycoris_lora came-pytorch scipy prodigy-plus-schedule-free && ").Append("accelerate config");
+            sb.Append("pip install dadaptation lycoris_lora came-pytorch scipy prodigy-plus-schedule-free && pip install numpy==1.26.4 && ").Append("accelerate config");
             ProcessStartInfo ps = new ProcessStartInfo();
             ps.FileName = "cmd";
             ps.Arguments = sb.ToString();
