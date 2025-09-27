5dd561c253ebf3875ce22f2624c2e38409fff518
RedRayz <71994877+RedRayz@users.noreply.github.com>
2025-09-27 15:59:50 +0900

    Remove some useless code

--- Kohya_lora_trainer/Constants.cs
+++ Kohya_lora_trainer/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Kohya_lora_trainer
+﻿namespace Kohya_lora_trainer
 {
     public class Constants
     {
